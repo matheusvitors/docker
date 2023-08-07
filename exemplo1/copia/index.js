2e303716a29bd2e3ad2c674dd9521bd01cb53a90
Matheus Vitor Sousa Almeida <matheus.almeida@trf1.jus.br>
2023-08-07 12:55:35 -0300

    docker

---
+++ exemplo1/copia/index.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+
+const app = express(); 
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Está funcionando!' })
+})
+
+app.listen(8000, () => {
+    console.log(`Server executando na porta ${port}`);
+})
